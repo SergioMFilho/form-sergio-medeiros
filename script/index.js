3981b34930a2f5c685bab1f908e3e2c93060e6f8
Sergio Medeiros <sergiof11@outlook.com.br>
2022-10-26 00:31:56 -0300

    Function to validate cpf implemented

---
+++ script/index.js
@@ -0,0 +1,151 @@
+const form = document.getElementById("form");
+const nome = document.getElementById("nome");
+
+function getValue(input) {
+  const cpf = document.getElementById("cpf");
+  let isValidCpf = validarCPF(input.value);
+  if (isValidCpf) {
+    setSuccessFor(cpf);
+  } else {
+    setErrorFor(cpf, "CPF inválido");
+  }
+  const cpfMascarado = CpfMask(input.value);
+  //   console.log(cpfMascarado);
+}
+
+function maskCpf(text) {
+  let maskedText = CpfMask(text.value);
+  document.getElementById("cpf").value = maskedText;
+  //   console.log(cpf.value);
+  //   cpf.value = CpfMask(text.value);
+}
+
+function validarCPF(cpf) {
+  cpf = cpf.replace(/[^\d]+/g, "");
+  if (cpf == "") return false;
+  // Elimina CPFs invalidos conhecidos
+  if (
+    cpf.length != 11 ||
+    cpf == "00000000000" ||
+    cpf == "11111111111" ||
+    cpf == "22222222222" ||
+    cpf == "33333333333" ||
+    cpf == "44444444444" ||
+    cpf == "55555555555" ||
+    cpf == "66666666666" ||
+    cpf == "77777777777" ||
+    cpf == "88888888888" ||
+    cpf == "99999999999"
+  )
+    return false;
+  // Valida 1o digito
+  add = 0;
+  for (i = 0; i < 9; i++) add += parseInt(cpf.charAt(i)) * (10 - i);
+  rev = 11 - (add % 11);
+  if (rev == 10 || rev == 11) rev = 0;
+  if (rev != parseInt(cpf.charAt(9))) return false;
+  // Valida 2o digito
+  add = 0;
+  for (i = 0; i < 10; i++) add += parseInt(cpf.charAt(i)) * (11 - i);
+  rev = 11 - (add % 11);
+  if (rev == 10 || rev == 11) rev = 0;
+  if (rev != parseInt(cpf.charAt(10))) return false;
+  return true;
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  checkInputs();
+});
+
+function checkInputs() {
+  const nome = nome.value.trim();
+
+  if (nome === "") {
+    // mostrar erro
+    // add classe
+    setErrorFor(nome, "Preencha esse campo");
+  } else {
+    // adicionar a classe de sucesso
+    setSuccessFor(nome);
+  }
+
+  //   if (emailValue === "") {
+  //     // mostrar erro
+  //     // add classe
+  //     setErrorFor(email, "Preencha esse campo");
+  //   } else if (!isEmail(emailValue)) {
+  //     setErrorFor(email, "Email inválido");
+  //   } else {
+  //     // adicionar a classe de sucesso
+  //     setSuccessFor(email);
+  //   }
+
+  //   if (passwordValue === "") {
+  //     // mostrar erro
+  //     // add classe
+  //     setErrorFor(password, "Preencha esse campo");
+  //   } else if (passwordValue.length < 8) {
+  //     setErrorFor(password, "Senha deve ter mais que 8 caracteres");
+  //   } else {
+  //     // adicionar a classe de sucesso
+  //     setSuccessFor(password);
+  //   }
+
+  //   if (passwordtwoValue === "") {
+  //     // mostrar erro
+  //     // add classe
+  //     setErrorFor(passwordtwo, "Preencha esse campo");
+  //   } else if (passwordValue !== passwordtwoValue) {
+  //     setErrorFor(passwordtwo, "Senhas não tão iguais");
+  //   } else {
+  //     // adicionar a classe de sucesso
+  //     setSuccessFor(passwordtwo);
+  //   }
+}
+
+function setErrorFor(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
+
+  small.innerText = message;
+
+  formControl.className = "form-control error";
+}
+
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+
+  formControl.className = "form-control success";
+}
+
+function CpfMask(num) {
+  let resultValue = "";
+  const numberValue = num.replace(/[\D]+/g, "").substring(0, 11);
+  for (let i = 0; numberValue.length > i; i++) {
+    if (i === 3) {
+      resultValue += `.${numberValue[i]}`;
+    } else if (i === 6) {
+      resultValue += `.${numberValue[i]}`;
+    } else if (i === 9) {
+      resultValue += `-${numberValue[i]}`;
+    } else {
+      resultValue += numberValue[i];
+    }
+  }
+  return resultValue;
+}
+
+function CepMask(num) {
+  let resultValue = "";
+  const numberValue = num.replace(/[\D]+/g, "").substring(0, 8);
+  for (let i = 0; numberValue.length > i; i++) {
+    if (i === 5) {
+      resultValue += `-${numberValue[i]}`;
+    } else {
+      resultValue += numberValue[i];
+    }
+  }
+  return resultValue;
+}
