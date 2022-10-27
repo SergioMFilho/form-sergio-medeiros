2a2d3f33dee4b1b2a9c9a5d093da0daec971efe2
Sergio Medeiros <sergiof11@outlook.com.br>
2022-10-27 09:59:53 -0300

    Birth date input validation

--- script/index.js
+++ script/index.js
@@ -94,7 +94,6 @@ function maskCpf(text) {
 function getValueNome(input) {
   const nome = document.getElementById("nome");
   let isValidNome = validarNome(input.value);
-  console.log(isValidNome);
   if (isValidNome) {
     setSuccessFor(nome);
   } else {
@@ -107,6 +106,53 @@ function validarNome(nome) {
     return true;
   } else {
     return false;
+  }
+}
+
+// function getValueDataNascimento(input) {
+//   const dataNascimento = document.getElementById("dataNascimento");
+//   let isValidDataNascimento = validarDataNascimento(input.value);
+//   if (isValidDataNascimento) {
+//     setSuccessFor(isValidDataNascimento);
+//   } else {
+//     setErrorFor(dataNascimento, "Nome inválido");
+//   }
+// }
+
+// function validarDataNascimento(dataNascimento) {
+//   if (dataNascimento) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+function validadata() {
+  const dataNascimento = document.getElementById("dataNascimento");
+  var data = document.getElementById("dataNascimento").value; // pega o valor do input
+  data = data.replace(/\//g, "-"); // substitui eventuais barras (ex. IE) "/" por hífen "-"
+  var data_array = data.split("-"); // quebra a data em array
+
+  // para o IE onde será inserido no formato dd/MM/yyyy
+  if (data_array[0].length != 4) {
+    data = data_array[2] + "-" + data_array[1] + "-" + data_array[0]; // remonto a data no formato yyyy/MM/dd
+  }
+
+  // comparo as datas e calculo a idade
+  var hoje = new Date();
+  var nasc = new Date(data);
+  var idade = hoje.getFullYear() - nasc.getFullYear();
+  var m = hoje.getMonth() - nasc.getMonth();
+  if (m < 0 || (m === 0 && hoje.getDate() < nasc.getDate())) idade--;
+
+  if (idade < 18) {
+    setErrorFor(dataNascimento, "Você precisa ter 18 anos ou mais");
+    return false;
+  }
+
+  if (idade >= 18 && idade <= 110) {
+    setSuccessFor(dataNascimento);
+    return true;
   }
 }
 
